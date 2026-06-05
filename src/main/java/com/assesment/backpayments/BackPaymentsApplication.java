a2a15d4836ddc7a825290f11eb843bb7ffa1ce98
samuel Barona <samuel.d.barona@gmail.com>
2026-06-04 21:48:32 -0500

    First commit

---
+++ src/main/java/com/assesment/backpayments/BackPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.assesment.backpayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPaymentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackPaymentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assesment/backpayments/BackPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assesment.backpayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPaymentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
